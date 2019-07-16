60975939cd29a472a867d3def92dd0f1ecf6aa91
Chidi Orji <orjichidi95@gmail.com>
2019-07-16 14:30:43 +0100

    refactor: convert to function components

--- src/layouts/AuthRoute.js
+++ src/layouts/AuthRoute.js
@@ -3,39 +3,29 @@ import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import ErrorBoundary from "../components/ErrorBoundary";
 
-class AuthRoute extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    const { component: Component, auth, ...rest } = this.props;
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          if (!auth.isAuthenticated) {
-            return (
-              <section className="login-layout-section">
-                <ErrorBoundary>
-                  <Component {...props} />
-                </ErrorBoundary>
-              </section>
-            );
-          } else {
-            return <Redirect to="/" />;
-          }
-        }}
-      />
-    );
-  }
-}
+const AuthRoute = ({ component: Component, auth, ...rest}) => {
+  return (
+    <Route {...rest} render={props => {
+      // if user is authenticated, redirect to homepage
+      if (auth.isAuthenticated) {
+        return <Redirect to="/" />;
+      }
+      else {
+        return (
+          <section className="login-layout-section">
+            <ErrorBoundary>
+              <Component {...props} />
+            </ErrorBoundary>
+          </section>
+        );
+      }
+    }}
+    />
+  );
+};
 
-const mapStateToProps = state => ({
-  auth: state.auth,
+const mapStateToProps = ({ auth }) => ({
+  auth,
 });
 
-export default connect(
-  mapStateToProps,
-  {}
-)(AuthRoute);
+export default connect(mapStateToProps)(AuthRoute);
--- src/layouts/PrivateRoute.js
+++ src/layouts/PrivateRoute.js
@@ -1,52 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import ErrorBoundary from "../components/ErrorBoundary";
 import Header from "./Header";
 
-class PrivateRoute extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+  return (
+    <Route {...rest} render={props => {
+      if (auth.isAuthenticated) {
+        return (
+          <ErrorBoundary>
+            <Header />
+            {/* <SideNav /> */}
+            <main id="main">
+              <Component {...props} />
+            </main>
+          </ErrorBoundary>
+        );
+      }
+      else {
+        //enable this line below to make route protected
+        return <Redirect to="/login" />;
+      }
+    }}
+    />
+  );
+};
 
-  componentDidMount() {}
-
-  componentDidUpdate(prevProps, prevState) {
-    //this.props.setActiveLink(this.props.path);
-  }
-
-  render() {
-    const { component: Component, auth, ...rest } = this.props;
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          if (auth.isAuthenticated) {
-            return (
-              <ErrorBoundary>
-                <Header />
-                {/* <SideNav /> */}
-                <main id="main">
-                  <Component {...props} />
-                </main>
-              </ErrorBoundary>
-            );
-          } else {
-            //enable this line below to make route protected
-            return <Redirect to="/login" />;
-          }
-        }}
-      />
-    );
-  }
-}
-
-const mapSateToProps = state => ({
-  auth: state.auth,
+const mapStateToProps = ({ auth }) => ({
+  auth,
 });
 
-export default connect(
-  mapSateToProps,
-  {}
-)(PrivateRoute);
+export default connect(mapStateToProps)(PrivateRoute);
